f8252443976c74710214e9270102c99977e32049
bryanvengwa <bmvengwa@gmail.com>
2024-03-22 21:19:36 +0200

    removed bug from the query action code

--- main.py
+++ main.py
@@ -3,62 +3,62 @@ from user_template import UserTemplate
 import numpy as np
 
 
-# Define a function to handle keystroke events
-def on_press(key):
-    try:
-        # Print the pressed key
-        print('Key {} pressed'.format(key))
-    except AttributeError:
-        # Ignore special keys
-        pass
+# # Define a function to handle keystroke events
+# def on_press(key):
+#     try:
+#         # Print the pressed key
+#         print('Key {} pressed'.format(key))
+#     except AttributeError:
+#         # Ignore special keys
+#         pass
 
 
-# Define a function to handle key release events
-def on_release(key):
-    # If the ESC key is pressed, stop the listener
-    if key == Key.esc:
-        return False
+# # Define a function to handle key release events
+# def on_release(key):
+#     # If the ESC key is pressed, stop the listener
+#     if key == Key.esc:
+#         return False
 
-# Create a listener to monitor keystrokes
-with Listener(on_press=on_press, on_release=on_release) as listener:
-    # Start listening for events
-    listener.join()
+# # Create a listener to monitor keystrokes
+# with Listener(on_press=on_press, on_release=on_release) as listener:
+#     # Start listening for events
+#     listener.join()
 
 
-if __name__ == '__main__':
-    # Create a UserTemplate instance
-    user_template = UserTemplate('user_features.db')
+# if __name__ == '__main__':
+#     # Create a UserTemplate instance
+#     user_template = UserTemplate('user_features.db')
 
-    # Connect to the database
-    user_template.connect_to_database()
+#     # Connect to the database
+#     user_template.connect_to_database()
 
-    # Create the table if it doesn't exist
-    user_template.create_table()
+#     # Create the table if it doesn't exist
+#     user_template.create_table()
 
-    # Sign up functionality: Register a user
-    user_id = user_template.generate_user_id()
-    print("Generated User ID:", user_id)
+#     # Sign up functionality: Register a user
+#     user_id = user_template.generate_user_id()
+#     print("Generated User ID:", user_id)
 
-    # Generate random paragraph for the user to type
-    paragraph = user_template.generate_random_paragraph()
-    print("Random Paragraph for User to Type:", paragraph)
+#     # Generate random paragraph for the user to type
+#     paragraph = user_template.generate_random_paragraph()
+#     print("Random Paragraph for User to Type:", paragraph)
 
-    # Simulate user features (random for demonstration)
-    user_features = np.random.rand(10)
-    print("Simulated User Features:", user_features)
+#     # Simulate user features (random for demonstration)
+#     user_features = np.random.rand(10)
+#     print("Simulated User Features:", user_features)
 
-    # Store user features into the database
-    user_template.store_user_feature(user_id, user_features)
-    print("User features stored in the database.")
+#     # Store user features into the database
+#     user_template.store_user_feature(user_id, user_features)
+#     print("User features stored in the database.")
 
-    # Close the database connection
-    user_template.close_connection()
+#     # Close the database connection
+#     user_template.close_connection()
 
 
 # if __name__ == '__main__':
 
 print("script is running ")
-def query_action(redo):
+def query_action(redo : bool ):
     valid_numbers = [1,2,3]
     if redo:
         print("Please enter a valid number")
@@ -68,14 +68,16 @@ def query_action(redo):
     print("2. Log a user into the system")
     print("3. Sign in a user")
     print("Enter the number of your option.......")
-    response = input()
-    if input not in valid_numbers:
-        response = input()
+    response =int(input())
+
+    if response not in valid_numbers:
         query_action(True)
+    else:    
+        print('you have entered the correct number')
 
     return response
 
-
+query_action(False)
 
     
     
