9b57203aef4d4b7b2f31d7eb5992b7026a7d1ebc
bryanvengwa <bmvengwa@gmail.com>
2024-05-15 10:51:19 +0200

    created use logiing in function

--- main.py
+++ main.py
@@ -2,32 +2,17 @@ from pynput.keyboard import Key, Listener
 from user_template import UserTemplate
 import numpy as np
 
-# from FeatureExtractor import FeatureExtractor
 
-# this is the oneTime overall connection to the userTemplate class
 template = UserTemplate('user_features.db')
-# features = FeatureExtractor('user_features.db')
 template.connect_to_database()
 template.create_table()
 
-# Define a function to handle keystroke events
-#
+def log_a_user():
+    print('Please enter your username')
+    
 
 
-# if __name__ == '__main__':
-#     user_id = template.generate_user_id()
-#     # user_name = input()
-#     template.register_user(user_id, 'bryanvengwa')
-#     print("Now type the paragraph below")
-#     paragraph = template.generate_paragraph()
-#     print(paragraph)
-#     template.start_capture(user_id)
-#     strokes = template.retrieve_user_keystrokes(user_id)
-#     print(strokes)
-#     # features.start_capture(user_id)
-#
-
-def register_user():
+def register_user(): 
     new_id = template.generate_user_id()
     print('Please Enter your username')
     user_name = input()
@@ -39,16 +24,13 @@ def register_user():
 
     template.start_capture(new_id)
     print('we are passing the id ' + new_id)
-    # features.start_capture(user_id)
 
-
-#     pass
-
-# print("script is running ")
 
 def validate_action(action):
     if action == 1:
         register_user()
+    elif action == 2:
+        log_a_user()
 
 
 def query_action(redo: bool):
